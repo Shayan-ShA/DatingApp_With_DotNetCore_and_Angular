f97b551f230efb2720f8c7e52ef10871c948e521
Shawn Sheikhakbari <sh.sheikhakbari@gmail.com>
2023-06-22 18:56:28 -0700

    DA#API_Walking_Skeleton_1.1

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 
 
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -14,6 +16,20 @@ namespace API.Controllers
         {
             _context = context;
 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
         }
     }
 }
