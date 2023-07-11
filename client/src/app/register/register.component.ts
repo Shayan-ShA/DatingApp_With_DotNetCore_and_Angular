383c3921b74dbdefd6cc36805927d5f7fcb14208
Shawn Sheikhakbari <sh.sheikhakbari@gmail.com>
2023-07-10 21:22:21 -0700

    DA#ClientLoginAndRegister_1.1

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,13 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
+  constructor(private accountService: AccountService) {}
+
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
   }
 
   cancel() {
-    console.log("cancelled");
+    this.cancelRegister.emit(false);
   }
 }
